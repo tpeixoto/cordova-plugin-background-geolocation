c1568c28f42973a36c60f144783fe6ba0febd650
Terence Santos <tpeixoto@2mundos.net>
2018-03-08 12:03:11 -0300

    Added log warning in start action.

--- android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
+++ android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
@@ -245,6 +245,8 @@ public class BackgroundGeolocationPlugin extends CordovaPlugin {
         if (ACTION_START.equals(action)) {
             executorService.execute(new Runnable() {
                 public void run() {
+                    log.warn("Execute start action");
+
                     if (config == null) {
                         log.warn("Attempt to start unconfigured service");
                         callbackContext.error("Plugin not configured. Please call configure method first.");
